de0ba5149ede1c14b3976d64943498606d18a16b
oleg <oleg@ya.ru>
2022-11-13 23:18:23 +0300

    First commit

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/', function() {
+    $data = [
+        'message' => "Welcome to our API"
+    ];
+    return response()->json($data, 200);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/user', [AuthController::class, 'getUser']);
+
+Route::middleware('jwt.verify')->group(function() {
+    Route::get('/dashboard', function() {
+        return response()->json(['message' => 'Welcome to dashboard'], 200);
+    });
+});
